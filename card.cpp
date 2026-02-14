51df89aa2ae662bba2603ff8fce0c448bc75a737
strifedeeno <coolarnay@gmail.com>
2026-02-13 18:45:20 -0800

    fix

--- card.cpp
+++ card.cpp
@@ -1,4 +1,80 @@
 // card.cpp
 // Author: Your name
 // Implementation of the classes defined in card.h
+#include "card.h"
+#include <string>
+CARD::CARD(char ty, std::string num) : suit(ty), numb(num)
+{
+    card = 0;
+    if (ty == 'd')
+    {
+        card += 13;
+    }
+    else if (ty == 's')
+    {
+        card += 26;
+    }
+    else if (ty == 'h')
+    {
+        card += 39;
+    }
 
+    if (num == "a")
+    {
+        card += 1;
+    }
+    else if (num == "j")
+    {
+        card += 11;
+    }
+    else if (num == "q")
+    {
+        card += 12;
+    }
+    else if (num == "k")
+    {
+        card += 13;
+    }
+    else if (num.length() == 2)
+    {
+        card += 10;
+    }
+    else
+    {
+        card += num[0] - '0';
+    }
+};
+
+int CARD::getCard() const
+{
+    return card;
+};
+bool CARD::operator>(const CARD &other) const
+{
+    if (card > other.getCard())
+    {
+        return true;
+    }
+    return false;
+};
+bool CARD::operator<(const CARD &other) const
+{
+    if (card < other.getCard())
+    {
+        return true;
+    }
+    return false;
+}
+bool CARD::operator==(const CARD &other) const
+{
+    if (card == other.getCard())
+    {
+        return true;
+    }
+    return false;
+}
+std::ostream &operator<<(std::ostream &os, const CARD &c)
+{
+    os << c.suit << " " << c.numb;
+    return os;
+}
--- card.h
+++ card.h
@@ -1,8 +1,28 @@
 // card.h
 // Author: Your name
-// All class declarations related to defining a single card go here
 
 #ifndef CARD_H
 #define CARD_H
+
+#include <string>
+#include <iostream>
+
+class CARD {
+private:
+    char suit;
+    std::string numb;
+    int card;
+
+public:
+    CARD(char ty, std::string num);
+
+    int getCard() const;
+
+    bool operator>(const CARD &other) const;
+    bool operator<(const CARD &other) const;
+    bool operator==(const CARD &other) const;
+
+    friend std::ostream& operator<<(std::ostream& os, const CARD& c);
+};
 
 #endif
